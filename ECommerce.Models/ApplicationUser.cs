683bcdbd00049782b25a5336138730f94965f646
Calance Hong <hyz081096@gmail.com>
2020-04-21 15:41:21 +0800

    Added ApplicationUser model

---
+++ ECommerce.Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ECommerce.Models
+{
+    class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Role { get; set; }
+    }
+}
