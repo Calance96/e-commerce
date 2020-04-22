4177901a04954df620015029cdcccc0da96cae34
Calance Hong <hyz081096@gmail.com>
2020-04-23 00:05:54 +0800

    Added session extension under utility

---
+++ ECommerce.Utility/SessionExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace ECommerce.Utility
+{
+    public static class SessionExtension
+    {
+        public static void SetObject(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonSerializer.Serialize(value));
+        }
+        public static T GetObject<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default(T) : JsonSerializer.Deserialize<T>(value, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
