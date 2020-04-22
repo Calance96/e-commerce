d8d2ab4ce094fcbaf8cc5ab83a12b7d4da60e135
Calance Hong <hyz081096@gmail.com>
2020-04-23 00:08:58 +0800

    Added DbSet for user, shopping carts, orders and order items

--- ECommerce.DataAccess/ApplicationDbContext.cs
+++ ECommerce.DataAccess/ApplicationDbContext.cs
@@ -14,5 +14,13 @@ namespace ECommerce.DataAccess
         public DbSet<Category> Categories { get; set; } 
 
         public DbSet<Product> Products { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        public DbSet<CartItem> CartItems { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderItem> OrderItems { get; set; }
     }
 }
