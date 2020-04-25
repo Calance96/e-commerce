b8d285f1787f1cadf2a1bd4327fac3cbaed3cd5d
Calance Hong <hyz081096@gmail.com>
2020-04-25 12:23:26 +0800

    - Remove unused constants - Added constants for PaymentStatus, OrderStatus and StripeChargeStatus

--- ECommerce.Utility/AppConstant.cs
+++ ECommerce.Utility/AppConstant.cs
@@ -7,12 +7,36 @@ namespace ECommerce.Utility
         public const string ROLE_ADMIN = "Admin";
         public const string ROLE_CUSTOMER = "Customer";
 
-        public const string MESSAGE_TYPE_SUCCESS = "Success";
-        public const string MESSAGE_TYPE_FAILURE = "Failure";
-
         public const string CART_SESSION_KEY = "EMallCartSession";
         public const string FULLNAME_SESSION_KEY = "FullName";
 
         public const string CONTENT_JSON = "application/json";
+
+        public static class PaymentStatus
+        {
+            public const string PENDING = "Pending";
+            public const string APPROVED = "Approved";
+            public const string REJECTED = "Rejected";
+            public const string REFUNDED = "Refunded";
+            public const string CANCELLED = "Cancelled";
+        }
+
+        public static class OrderStatus
+        {
+            public const string PENDING = "Pending";
+            public const string APPROVED = "Approved";
+            public const string PROCESSING = "Processing";
+            public const string SHIPPED = "Shipped";
+            public const string CANCELLED = "Cancelled";
+            public const string COMPLETE = "Completed";
+            public const string REFUNDED = "Refunded";
+        }
+
+        public static class StripeChargeStatus
+        {
+            public const string SUCCEEDED = "succeeded";
+            public const string PENDING = "pending";
+            public const string FAILED = "failed";
+        }
     }
 }
