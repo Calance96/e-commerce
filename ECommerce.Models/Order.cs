8c4476a57a9525cd0dca2da67a25b11f31ab287f
Calance Hong <hyz081096@gmail.com>
2020-04-25 12:26:36 +0800

    Added: - Display Name for OrderDate - PaymentDate attribute - Display Name for ShipDate - Display Name for TrackingNumber - PaymentStatus attribute - TransactionId attribute - Email attribute - Display Name for ShippingAddress - Display Name for BillingAddress - Display Name for PhoneNumber

--- ECommerce.Models/Order.cs
+++ ECommerce.Models/Order.cs
@@ -9,6 +9,7 @@ namespace ECommerce.Models
     public class Order
     {
         [Key]
+        [Display(Name = "Order ID")]
         public long Id { get; set; }
 
         public string UserId { get; set; }
@@ -17,28 +18,51 @@ namespace ECommerce.Models
         public ApplicationUser User { get; set; }
 
         [Required]
+        [Display(Name = "Order Date")]
         public DateTime OrderDate { get; set; }
+
+        [Display(Name = "Payment Date")]
+        public DateTime PaymentDate { get; set; }
         
         [Required]
+        [Display(Name = "Shipped On")]
         public DateTime ShipDate { get; set; }
 
         [Required]
         [Column(TypeName = "decimal(18,2)")]
         public decimal OrderTotal { get; set; }
 
+        [Display(Name = "Tracking No.")]
         public string TrackingNumber { get; set; }
 
         public string Carrier { get; set; }
 
+        [Display(Name = "Order Status")]
         public string OrderStatus { get; set; }
+
+        [Display(Name = "Payment Status")]
+        public string PaymentStatus { get; set; }
+
+        [Display(Name = "Transaction ID")]
+        public string TransactionId { get; set; }
 
         [Required]
         public string Name { get; set; }
+
         [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Shipping Address")]
         public string ShippingAddress { get; set; }
+
         [Required]
+        [Display(Name = "Billing Address")]
         public string BillingAddress { get; set; }
+
         [Required]
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
     }
 }
