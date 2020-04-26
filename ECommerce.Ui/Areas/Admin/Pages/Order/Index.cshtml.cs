7976ecd23bb9f5fa23c7a6a93dd7708bb85ade10
Calance Hong <hyz081096@gmail.com>
2020-04-26 15:29:22 +0800

    Remove unused using statement

--- ECommerce.Ui/Areas/Admin/Pages/Order/Index.cshtml.cs
+++ ECommerce.Ui/Areas/Admin/Pages/Order/Index.cshtml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ECommerce.Ui.Services;
+using ECommerce.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
