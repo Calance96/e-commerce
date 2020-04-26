1fa197c3f818978323ab434c8fd75047c88becd3
Calance Hong <hyz081096@gmail.com>
2020-04-26 15:26:58 +0800

    Register now calls API

--- ECommerce.Ui/Areas/Account/Pages/Register.cshtml.cs
+++ ECommerce.Ui/Areas/Account/Pages/Register.cshtml.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using ECommerce.Models;
 using ECommerce.Models.ViewModels;
+using ECommerce.Ui.Services;
 using ECommerce.Utility;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -13,17 +16,12 @@ namespace ECommerce.Ui.Areas.Account.Pages
 {
     public class RegisterModel : PageModel
     {
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly SignInManager<ApplicationUser> _signInManager;
-        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly AuthService _authService;
 
-        public RegisterModel(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
+        public RegisterModel(AuthService authService)
         {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _roleManager = roleManager;
+            _authService = authService;
         }
-
 
         [BindProperty]
         public RegisterViewModel Input { get; set; }
@@ -33,41 +31,35 @@ namespace ECommerce.Ui.Areas.Account.Pages
             returnUrl ??= Url.Content("~/");
             if (ModelState.IsValid)
             {
-                ApplicationUser user = new ApplicationUser
+                var authResult = await _authService.Register(Input);
+                
+                switch (authResult.StatusCode)
                 {
-                    UserName = Input.Email,
-                    Email = Input.Email,
-                    Address = Input.Address,
-                    Name = Input.Name,
-                    PhoneNumber = Input.PhoneNumber,
-                    Role = Input.Role
-                };
+                    case SD.StatusCode.OK:
+                        var claims = new List<Claim>
+                        {
+                            new Claim(ClaimTypes.NameIdentifier, authResult.ApplicationUser.Id),
+                            new Claim(ClaimTypes.Name, authResult.ApplicationUser.Name),
+                            new Claim(ClaimTypes.Email, authResult.ApplicationUser.Email),
+                            new Claim(ClaimTypes.Role, authResult.ApplicationUser.Role),
+                            new Claim(ClaimTypes.MobilePhone, authResult.ApplicationUser.PhoneNumber),
+                        };
 
-                var result = await _userManager.CreateAsync(user, Input.Password);
-                if (result.Succeeded)
-                {
-                    if (!await _roleManager.RoleExistsAsync(SD.ROLE_ADMIN))
-                    {
-                        await _roleManager.CreateAsync(new IdentityRole(SD.ROLE_ADMIN));
-                    }
-                    if (!await _roleManager.RoleExistsAsync(SD.ROLE_CUSTOMER))
-                    {
-                        await _roleManager.CreateAsync(new IdentityRole(SD.ROLE_CUSTOMER));
-                    }
+                        var claimsIdentity = new ClaimsIdentity(claims, "Password");
+                        var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+                        await HttpContext.SignInAsync(IdentityConstants.ApplicationScheme, new ClaimsPrincipal(claimsIdentity), new AuthenticationProperties
+                        {
+                            IsPersistent = true,
+                            RedirectUri = returnUrl
+                        });
 
-                    await _userManager.AddToRoleAsync(user, SD.ROLE_CUSTOMER);
-
-                    var signInResult = await _signInManager.PasswordSignInAsync(user, Input.Password, false, false);
-                    
-                    if (signInResult.Succeeded)
-                    {
                         return LocalRedirect(returnUrl);
-                    }
-                }
-
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
+                    case SD.StatusCode.BAD_REQUEST:
+                        foreach (var error in authResult.Message)
+                        {
+                            ModelState.AddModelError(string.Empty, error);
+                        }
+                        break;
                 }
             }
             return Page();
