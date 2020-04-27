09cba1833872c5d6c6ac405fc36c36f263430ddf
Calance Hong <hyz081096@gmail.com>
2020-04-27 14:08:48 +0800

    Added pagination to home page products display.

--- ECommerce.Ui/Pages/Index.cshtml.cs
+++ ECommerce.Ui/Pages/Index.cshtml.cs
@@ -7,6 +7,7 @@ using ECommerce.Ui.Services;
 using ECommerce.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 
 namespace ECommerce.Ui.Pages
@@ -14,17 +15,45 @@ namespace ECommerce.Ui.Pages
     public class IndexModel : PageModel
     {
         private readonly ProductService _productService;
+        private readonly CategoryService _categoryService;
 
-        public IndexModel(ProductService productService)
+        public string SearchTerm { get; set; }
+        public string CategoryFilter { get; set; }
+        public List<SelectListItem> Categories { get; set; }
+
+        public IndexModel(ProductService productService, CategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
-        public IEnumerable<Product> Products { get; set; }
+        public PaginatedList<Product> Products { get; set; }
 
-        public async Task OnGet()
+        public async Task OnGet(string searchString, int? pageIndex, string category)
         {
-            Products = await _productService.GetAll();
+            var ProductsFromDb = await _productService.GetAll();
+            Categories = (await _categoryService.GetAll()).Select(x => new SelectListItem
+            {
+                Text = x.Name,
+                Value = x.Name,
+                Selected = (x.Name == category? true : false)
+            }).ToList(); 
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                SearchTerm = searchString.ToLower();
+                ProductsFromDb = ProductsFromDb.Where(p => p.Name.ToLower().Contains(SearchTerm));
+            }
+
+            if (!string.IsNullOrEmpty(category))
+            {
+                CategoryFilter = category;
+                ProductsFromDb = ProductsFromDb.Where(p => p.Category.Name == category);
+            }
+
+            int pageSize = 8;
+            Products = await PaginatedList<Product>.CreateAsync(ProductsFromDb.AsQueryable<Product>(), pageIndex ?? 1, pageSize);
+
         }
     }
 }
