b54d0a07a2066c063d4d4f20cbb000a8b6c64226
Calance Hong <hyz081096@gmail.com>
2020-04-27 16:49:15 +0800

    Added search function and pagination display to admin product section.

--- ECommerce.Ui/Areas/Admin/Pages/Product/Index.cshtml.cs
+++ ECommerce.Ui/Areas/Admin/Pages/Product/Index.cshtml.cs
@@ -3,27 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ECommerce.Ui.Services;
+using ECommerce.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ECommerce.Ui.Areas.Admin.Pages.Product
 {
     public class IndexModel : PageModel
     {
         private readonly ProductService _productService;
-        public IndexModel(ProductService productService)
+        private readonly CategoryService _categoryService;
+
+        public IndexModel(ProductService productService, CategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
-        public IEnumerable<Models.Product> Products { get; set; }
+        public string SearchTerm { get; set; }
+        public string CategoryFilter { get; set; }
+        public List<SelectListItem> Categories { get; set; }
+
+        public PaginatedList<Models.Product> Products { get; set; }
 
         [TempData]
         public string Message { get; set; }
 
-        public async Task OnGet()
+        public async Task OnGet(string searchString, string category, int? pageIndex)
         {
-            Products = await _productService.GetAll();
+            var ProductsFromDb = await _productService.GetAll();
+            CategoryFilter = category ?? "All";
+            SearchTerm = searchString ?? "";
+
+            Categories = (await _categoryService.GetAll()).Select(x => new SelectListItem
+            {
+                Text = x.Name,
+                Value = x.Name,
+                Selected = (x.Name == category ? true : false)
+            }).ToList();
+
+            Categories.Add(new SelectListItem
+            {
+                Text = "All",
+                Value = "All",
+                Selected = (CategoryFilter == "All" ? true : false)
+            });
+
+            if (!string.IsNullOrEmpty(SearchTerm))
+            {
+                ProductsFromDb = ProductsFromDb.Where(p => p.Name.ToLower().Contains(SearchTerm.ToLower()));
+            }
+
+            if (!string.IsNullOrEmpty(category) && CategoryFilter != "All")
+            {
+                ProductsFromDb = ProductsFromDb.Where(p => p.Category.Name == category);
+            }
+
+            int pageSize = 10;
+            Products = await PaginatedList<Models.Product>.CreateAsync(ProductsFromDb.AsQueryable<Models.Product>(), pageIndex ?? 1, pageSize);
         }
 
         public async Task<IActionResult> OnPostDelete(long id)
