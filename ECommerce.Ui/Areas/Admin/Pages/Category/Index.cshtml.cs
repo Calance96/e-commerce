f5aa621914341e8d991023bde0f5f285b94508d1
Calance Hong <hyz081096@gmail.com>
2020-04-28 19:56:27 +0800

    Added pagination and search feature to category management (admin)

--- ECommerce.Ui/Areas/Admin/Pages/Category/Index.cshtml.cs
+++ ECommerce.Ui/Areas/Admin/Pages/Category/Index.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ECommerce.Ui.Services;
+using ECommerce.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -12,7 +13,11 @@ namespace ECommerce.Ui.Areas.Admin.Pages.Category
     {
         private readonly CategoryService _categoryService;
 
-        public IEnumerable<Models.Category> Categories { get; set; } = new List<Models.Category>();
+        public PaginatedList<Models.Category> Categories { get; set; }
+
+        public string SearchTerm { get; set; }
+
+        private const int PAGE_SIZE = 10;
 
         [TempData]
         public string ErrorMessage { get; set; }
@@ -22,26 +27,29 @@ namespace ECommerce.Ui.Areas.Admin.Pages.Category
             _categoryService = categoryService;
         }
 
-        public async Task OnGet()
+        public async Task OnGet(string searchString, int? pageIndex)
         {
-            Categories = await _categoryService.GetAll();
+            SearchTerm = searchString ?? "";
+
+            var CategoriesFromDb = await _categoryService.GetAll();
+
+            if (!string.IsNullOrEmpty(SearchTerm))
+            {
+                searchString = searchString.ToLower();
+                CategoriesFromDb = CategoriesFromDb.Where(c => c.Name.ToLower().Contains(searchString));
+            }
+
+            Categories = await PaginatedList<Models.Category>.CreateAsync(CategoriesFromDb.AsQueryable<Models.Category>(), pageIndex ?? 1, PAGE_SIZE);
         }
 
-        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        public async Task<IActionResult> OnPostDeleteAsync(int id, string searchString, int? pageIndex)
         {
-            try
+            var success = await _categoryService.Delete(id);
+            if (!success)
             {
-                var success = await _categoryService.Delete(id);
-                if (!success)
-                {
-                    ErrorMessage = "Category cannot be deleted as there are products belong to this category.";
-                }
-                return RedirectToPage("Index");
+                ErrorMessage = "Category cannot be deleted as there are products belong to this category.";
             }
-            catch
-            {
-                return BadRequest();
-            }
+            return RedirectToPage("Index", new { searchString, pageIndex });
         }
     }
 }
